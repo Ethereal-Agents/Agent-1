7937b162dbd57dadedb8ed6eecc0c886abfae518
Trinetra Devkatte <tridev09.td@gail.com>
2026-06-26 17:31:05 +0530

    chore: remove scratchpad scripts

--- query_golden.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-ds = load_dataset("princeton-nlp/SWE-bench_Verified", split="test")
-for row in ds:
-    if row["instance_id"] == "matplotlib__matplotlib-26466":
-        print(row["patch"])
-        break
